7c7bfc2a9b2e3fc7b3cd0e8b87096d88adf8b26e
TenTakano <takano20771@gmail.com>
2021-02-12 02:01:56 +0900

    impl test for get_instance_status

--- test/minecraft_controller/ec2_test.exs
+++ test/minecraft_controller/ec2_test.exs
@@ -3,49 +3,63 @@ defmodule MinecraftController.Context.EC2Test do
 
   alias MinecraftController.Context.EC2
 
-  describe "target_instance_id/0" do
-    test "returns instance_id" do
-      instance_id = "some_instance_id"
-      :meck.expect(ExAws, :request!, fn _ ->
-        body =
-          """
-          <DescribeInstancesResponse>
-            <reservationSet>
+  setup do
+    instance_id = "some_instance_id"
+    xml = """
+      <DescribeInstancesResponse>
+        <reservationSet>
+          <item>
+            <instanceId>#{instance_id}</instanceId>
+            <tagSet>
               <item>
-                <instanceId>#{instance_id}</instanceId>
-                <tagSet>
-                  <item>
-                    <key>Name</key>
-                    <value>Minecraft</value>
-                  </item>
-                  <item>
-                    <key>controller</key>
-                    <value>minecraft-controller</value>
-                  </item>
-                </tagSet>
+                <key>Name</key>
+                <value>Minecraft</value>
               </item>
-            </reservationSet>
-          </DescribeInstancesResponse>
-          """
-        %{status_code: 200, body: body}
-      end)
+              <item>
+                <key>controller</key>
+                <value>minecraft-controller</value>
+              </item>
+            </tagSet>
+            <instanceState>
+              <code>80</code>
+              <name>stopped</name>
+            </instanceState>
+          </item>
+        </reservationSet>
+      </DescribeInstancesResponse>
+      """
+    xml_without_instance = """
+      <DescribeInstancesResponse>
+        <reservationSet>
+        </reservationSet>
+      </DescribeInstancesResponse>
+      """
 
+    [instance_id: instance_id, xml: xml, xml_without_instance: xml_without_instance]
+  end
+
+  describe "target_instance_id/0" do
+    test "returns instance_id", %{instance_id: instance_id, xml: xml} do
+      :meck.expect(ExAws, :request!, fn _ -> %{status_code: 200, body: xml} end)
       assert EC2.target_instance_id() == instance_id
     end
 
-    test "returns nil if target instance doesn't exist" do
-      :meck.expect(ExAws, :request!, fn _ ->
-        body =
-          """
-          <DescribeInstancesResponse>
-            <reservationSet>
-            </reservationSet>
-          </DescribeInstancesResponse>
-          """
-        %{status_code: 200, body: body}
-      end)
-
+    test "returns nil if target instance doesn't exist", %{xml_without_instance: xml_without_instance} do
+      :meck.expect(ExAws, :request!, fn _ -> %{status_code: 200, body: xml_without_instance} end)
       assert EC2.target_instance_id() |> is_nil()
+    end
+  end
+
+  describe "get_instance_status/1" do
+    test "returns instance status", %{instance_id: instance_id, xml: xml} do
+      :meck.expect(ExAws, :request!, fn _ -> %{status_code: 200, body: xml} end)
+      assert EC2.get_instance_status(instance_id) == "stopped"
+    end
+
+    test "returns nil if target instance doesn't exist", ctx do
+      %{instance_id: instance_id, xml_without_instance: xml_without_instance} = ctx
+      :meck.expect(ExAws, :request!, fn _ -> %{status_code: 200, body: xml_without_instance} end)
+      assert EC2.get_instance_status(instance_id) |> is_nil()
     end
   end
 end
