18743ea8b6ba74f85726888c8d2b5bf1fcd2acc7
TenTakano <takano20771@gmail.com>
2021-02-12 01:08:26 +0900

    revise data_case.ex

--- test/support/data_case.ex
+++ test/support/data_case.ex
@@ -9,29 +9,8 @@ defmodule MinecraftController.DataCase do
     end
   end
 
-  setup _tags do
-    # :ok = Ecto.Adapters.SQL.Sandbox.checkout(MinecraftController.Repo)
-
-    # unless tags[:async] do
-    #   Ecto.Adapters.SQL.Sandbox.mode(MinecraftController.Repo, {:shared, self()})
-    # end
-
+  setup do
+    on_exit(&:meck.unload/0)
     :ok
   end
-
-  @doc """
-  A helper that transforms changeset errors into a map of messages.
-
-      assert {:error, changeset} = Accounts.create_user(%{password: "short"})
-      assert "password is too short" in errors_on(changeset).password
-      assert %{password: ["password is too short"]} = errors_on(changeset)
-
-  """
-  # def errors_on(changeset) do
-  #   Ecto.Changeset.traverse_errors(changeset, fn {message, opts} ->
-  #     Regex.replace(~r"%{(\w+)}", message, fn _, key ->
-  #       opts |> Keyword.get(String.to_existing_atom(key), key) |> to_string()
-  #     end)
-  #   end)
-  # end
 end
