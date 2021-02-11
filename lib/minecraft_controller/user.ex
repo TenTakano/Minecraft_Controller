b4ee6591a3f7a9e6ecb85142f7a5dbf4b2a7ae38
TenTakano <takano20771@gmail.com>
2021-02-11 16:47:01 +0900

    impl get user function

--- lib/minecraft_controller/user.ex
+++ lib/minecraft_controller/user.ex
@@ -1,10 +1,22 @@
-defmodule MinecraftController.Context.User do
+defmodule MinecraftController.Context.Users do
+  alias ExAws.Dynamo
   alias MinecraftController.Auth
 
-  # TODO: Need to be replaced with dynamoDB implementation
-  def get_user(_user_id) do
-    hash = Auth.hash_password("password", "somesolt")
-    
-    {:ok, %{id: "some_id", password_hash: hash, salt: "somesolt"}}
+  defmodule User do
+    @derive [Dynamo.Encodable]
+    defstruct [:id, :password_hash, :salt]
+  end
+
+  @table_name "Users"
+
+  @spec get_user(String.t) :: {:ok, User.t} | {:error, :not_found}
+  def get_user(user_id) do
+    Dynamo.get_item(@table_name, %{id: user_id})
+    |> ExAws.request!()
+    |> Dynamo.decode_item(as: User)
+    |> case do
+      %{id: id} when is_nil(id) -> {:error, :not_found}
+      user -> {:ok, user}
+    end
   end
 end
