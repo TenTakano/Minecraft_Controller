a5ab0de761e7eb45f5870b8fb27afc4fcabd054f
TenTakano <takano20771@gmail.com>
2021-02-11 17:27:49 +0900

    fix test

--- test/minecraft_controller_web/controllers/user_controller/login_test.exs
+++ test/minecraft_controller_web/controllers/user_controller/login_test.exs
@@ -1,5 +1,10 @@
-defmodule MinevraftControllerWeb.UserController.LoginTest do
+defmodule MinecraftControllerWeb.UserController.LoginTest do
   use MinecraftControllerWeb.ConnCase
+
+  alias ExCrypto.Token
+  alias MinecraftController.Auth
+  alias MinecraftController.Context.Users
+  alias Users.User
 
   describe "post/2" do
     setup %{conn: conn} do
@@ -7,10 +12,18 @@ defmodule MinevraftControllerWeb.UserController.LoginTest do
     end
 
     test "generates access token from user attributes and returns it", %{conn: conn} do
-      req_body = %{id: "some_id", password: "password"}
+      user = %User{
+        id: "someone",
+        salt: "somesalt",
+        password_hash: Auth.hash_password("password", "somesalt")
+      }
+      :meck.expect(Users, :get_user, fn _ -> {:ok, user} end)
+      :meck.expect(Token, :create!, fn (_, _) -> "sometoken" end)
+      expected = %{"token" => "sometoken"}
 
+      req_body = %{id: "some_id", password: "password"}
       assert %{status: 200, resp_body: body} = post(conn, "/api/users/login", req_body)
-      assert %{"token" => _} = Jason.decode!(body)
+      assert Jason.decode!(body) == expected
     end
   end
 end
