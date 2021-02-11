0b61342e340515f327c0165dbb895dde9043db30
TenTakano <takano20771@gmail.com>
2021-02-12 02:19:01 +0900

    impl wait function for ec2 startup

--- lib/minecraft_controller_web/controllers/status_controller/start.ex
+++ lib/minecraft_controller_web/controllers/status_controller/start.ex
@@ -3,13 +3,39 @@ defmodule MinecraftControllerWeb.EC2Controller.Start do
 
   alias MinecraftController.Context.EC2
 
+  @retry_interval_milliseconds 1000
+  @retry_times_limit 10
+
   def get(conn, _) do
-    case EC2.target_instance_id() do
-      nil ->
-        json(conn, %{status: "nothing"})
-      instance_id ->
-        :ok = EC2.start_instance(instance_id)
-        json(conn, %{instance_id: instance_id, status: "running"})
+    with(
+      {:ok, instance_id} <- EC2.target_instance_id(),
+      :ok <- EC2.start_instance(instance_id),
+      :ok <- wait_for_instance_started(instance_id)
+    )
+    do
+      res_body = %{
+        instance_id: instance_id,
+        status: "running"
+      }
+      json(conn, res_body)
+    else
+      {:error, :not_found} -> conn |> put_status(400) |> json(%{message: "Instance not found"})
+      :timeout -> conn |> put_status(409) |> json(%{message: "Something error occurs on AWS"})
+    end
+  end
+
+  @spec wait_for_instance_started(String.t) :: :ok | :timeout
+  defp wait_for_instance_started(instance_id), do: wait_for_instance_started(instance_id, 0)
+
+  @spec wait_for_instance_started(String.t, number) :: :ok | :timeout
+  defp wait_for_instance_started(instance_id, retry_times) do
+    case {EC2.get_instance_status(instance_id), retry_times} do
+      {"running", _} -> :ok
+      {_, retry_times} when retry_times < @retry_times_limit ->
+        :timer.sleep(@retry_interval_milliseconds)
+        wait_for_instance_started(instance_id)
+      _ ->
+        :timeout
     end
   end
 end
--- test/minecraft_controller_web/controllers/ec2_controller/start_test.exs
+++ test/minecraft_controller_web/controllers/ec2_controller/start_test.exs
@@ -5,8 +5,9 @@ defmodule MinecraftControllerWeb.EC2Controller.StartTest do
 
   describe "get/2" do
     test "execute start script", %{conn: conn} do
-      :meck.expect(EC2, :target_instance_id, fn -> "started_instance_id" end)
+      :meck.expect(EC2, :target_instance_id, fn -> {:ok, "started_instance_id"} end)
       :meck.expect(EC2, :start_instance, fn _ -> :ok end)
+      :meck.expect(EC2, :get_instance_status, fn _ -> "running" end)
       expected = %{"instance_id" => "started_instance_id", "status" => "running"}
 
       assert %{status: 200, resp_body: body} = get(conn, "/api/ec2/start")
