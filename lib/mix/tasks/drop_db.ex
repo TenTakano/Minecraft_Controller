8439bd92c6ac01e67be7a9eff7724d4cfa874fde
TenTakano <takano20771@gmail.com>
2021-02-11 16:11:34 +0900

    impl a mix task to drop dbs

---
+++ lib/mix/tasks/drop_db.ex
@@ -0,0 +1,14 @@
+defmodule Mix.Tasks.DropDb do
+  use Mix.Task
+
+  alias ExAws.Dynamo
+
+  def run(_) do
+    Application.ensure_all_started(:hackney)
+
+    %{"TableNames" => table_names} = Dynamo.list_tables() |> ExAws.request!()
+    Enum.each(table_names, fn table ->
+      {:ok, _} = Dynamo.delete_table(table) |> ExAws.request()
+    end)
+  end
+end
