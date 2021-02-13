5a491e937b6c5a63c9c8c281607a8371a4d1ad69
TenTakano <takano20771@gmail.com>
2021-02-13 15:49:52 +0900

    refactor error response on Login API

--- lib/minecraft_controller_web/controllers/error.ex
+++ lib/minecraft_controller_web/controllers/error.ex
@@ -11,11 +11,27 @@ defmodule MinecraftControllerWeb.Error do
     end
   end
 
+  defmodule BadRequest do
+    use ErrorBase, %{
+      status: 400,
+      type: "BadRequest",
+      message: "Given parameters were invalid."
+    }
+  end
+
+  defmodule ResourceNotFound do
+    use ErrorBase, %{
+      status: 404,
+      type: "ResourceNotFound",
+      message: "Specified resource does not exist."
+    }
+  end
+
   defmodule InstanceNotFound do
     use ErrorBase, %{
       status: 404,
       type: "InstanceNotFound",
-      message: "EC2 instance has not been setup"
+      message: "EC2 instance has not been setup."
     }
   end
 
@@ -23,7 +39,7 @@ defmodule MinecraftControllerWeb.Error do
     use ErrorBase, %{
       status: 409,
       type: "AWSError",
-      message: "Something occurs on AWS"
+      message: "Something occurs on AWS."
     }
   end
 end
