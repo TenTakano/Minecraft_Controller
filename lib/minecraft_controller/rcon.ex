ba139714e61d1fc048bf7e81fc947b585748cc68
TenTakano <takano20771@gmail.com>
2021-02-14 19:20:14 +0900

    impl get player list function

--- lib/minecraft_controller/rcon.ex
+++ lib/minecraft_controller/rcon.ex
@@ -1,2 +1,26 @@
 defmodule MinecraftController.RCON do
+  alias __MODULE__.{Client, Packet}
+
+  def get_player_list() do
+    gen_command("list")
+    |> Client.send_command()
+    |> case do
+      {:ok, %{payload: response_message}} ->
+        res =
+          ~r/There are (?<count>\d*) of a max of (?<limit>\d*) players online: (?<members>.*)/
+          |> Regex.named_captures(response_message)
+          |> Map.update!("members", fn
+            "" -> []
+            members -> String.split(members, "")
+          end)
+        {:ok, res}
+      error ->
+        error
+    end
+  end
+
+  @spec gen_command(String.t) :: Packet.t
+  defp gen_command(op) do
+    Packet.encode(%{id: 2, type: :command, payload: op})
+  end
 end
