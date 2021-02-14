bf3f4268cc15d2f3954e2ce11e75338ae9122b4f
TenTakano <takano20771@gmail.com>
2021-02-14 20:59:55 +0900

    impl mix task for check and stop server

--- lib/mix/tasks/check_and_stop_server.ex
+++ lib/mix/tasks/check_and_stop_server.ex
@@ -2,14 +2,16 @@ defmodule Mix.Tasks.CheckAndStopServer do
   use Mix.Task
 
   alias MinecraftController.RCON
+  alias MinecraftController.Context.EC2
 
   def run(_) do
     with(
       {:ok, player_list} <- RCON.get_player_list(),
       true <- is_able_to_shutdown(player_list),
-      {:ok, _} <- RCON.stop_server()
+      {:ok, _} <- RCON.stop_server(),
+      {:ok, instance_id} <- EC2.target_instance_id()
     ) do
-      :shutdown_ec2
+      EC2.stop_instance(instance_id)
     else
       false -> :ok
     end
