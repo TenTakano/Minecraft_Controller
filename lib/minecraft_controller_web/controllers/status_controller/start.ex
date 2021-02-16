1ffaf963e8e62cfb716a5cef80db9de8e06b6d1b
TenTakano <takano20771@gmail.com>
2021-02-17 01:29:58 +0900

    update start server api

--- lib/minecraft_controller_web/controllers/status_controller/start.ex
+++ lib/minecraft_controller_web/controllers/status_controller/start.ex
@@ -11,27 +11,23 @@ defmodule MinecraftControllerWeb.EC2Controller.Start do
     with(
       {:ok, instance_id} <- EC2.target_instance_id(),
       :ok <- EC2.start_instance(instance_id),
-      :ok <- wait_for_instance_started(instance_id)
+      ip <- wait_for_instance_started(instance_id)
     )
     do
-      res_body = %{
-        instance_id: instance_id,
-        status: "running"
-      }
-      json(conn, res_body)
+      json(conn, %{ip: ip})
     else
       {:error, :not_found} -> error_json(conn, Error.InstanceNotFound)
       :timeout -> error_json(conn, Error.AwsError)
     end
   end
 
-  @spec wait_for_instance_started(String.t) :: :ok | :timeout
+  @spec wait_for_instance_started(String.t) :: String.t | :timeout
   defp wait_for_instance_started(instance_id), do: wait_for_instance_started(instance_id, 0)
 
-  @spec wait_for_instance_started(String.t, number) :: :ok | :timeout
+  @spec wait_for_instance_started(String.t, number) :: String.t | :timeout
   defp wait_for_instance_started(instance_id, retry_times) do
     case {EC2.get_instance_status(instance_id), retry_times} do
-      {"running", _} -> :ok
+      {%{status: "running", ip: ip}, _} -> ip
       {_, retry_times} when retry_times < @retry_times_limit ->
         Utils.wait_milliseconds(@retry_interval_milliseconds)
         wait_for_instance_started(instance_id, retry_times + 1)
