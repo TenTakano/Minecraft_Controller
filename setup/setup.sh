a25b60f38dad81edec264a76893d3bc6be26c3fa
TenTakano <takano20771@gmail.com>
2021-02-16 23:58:42 +0900

    change default memory size

--- setup/setup.sh
+++ setup/setup.sh
@@ -6,7 +6,7 @@ docker pull itzg/minecraft-server
 docker run -d --name=minecraft \
   -p 25565:25565 -p 25575:25575 \
   -v /home/minecraft:/data \
-  -e EULA=TRUE -e ENABLE_RCON=true -e RCON_PASSWORD=pass -e MEMORY=500M \
+  -e EULA=TRUE -e ENABLE_RCON=true -e RCON_PASSWORD=pass -e MEMORY=1G \
   itzg/minecraft-server
 docker stop minecraft
 
