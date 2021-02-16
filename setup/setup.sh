7c2e39cadcd0a62bfcbad35dce5dd519f3baef43
TenTakano <takano20771@gmail.com>
2021-02-17 01:10:25 +0900

    5 impl minecraft service (#24)
    Added systemd service file to start minecraft server
    Added setup script for minecraft EC2 instance

---
+++ setup/setup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+mkdir /home/minecraft
+
+docker pull itzg/minecraft-server
+docker run -d --name=minecraft \
+  -p 25565:25565 -p 25575:25575 \
+  -v /home/minecraft:/data \
+  -e EULA=TRUE -e ENABLE_RCON=true -e RCON_PASSWORD=pass -e MEMORY=1G \
+  itzg/minecraft-server
+docker stop minecraft
+
+cp ./minecraft.service /lib/systemd/system
+chmod 744 /lib/systemd/system/minecraft.service
+systemctl daemon-reload
+systemctl enable minecraft
