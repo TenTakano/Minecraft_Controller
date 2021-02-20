4d2e588d4ba912e7627a7f9cc5c819e2ced02689
TenTakano <takano20771@gmail.com>
2021-02-20 22:53:51 +0900

    impl test for token validator

---
+++ test/minecraft_controller_web/plug/verify_api_token_test.exs
@@ -0,0 +1,28 @@
+defmodule MinecraftControllerWeb.Plug.VerifyApiTokenTest do
+  use MinecraftController.CommonCase
+
+  alias Plug.Conn
+  alias MinecraftController.Auth
+
+  describe "call/2" do
+    setup do
+      conn = Phoenix.ConnTest.build_conn()
+      token = Auth.gen_access_token(%{id: "someone"})
+      [conn: conn, token: token]
+    end
+
+    test "assigns valid api token", %{conn: conn_base, token: token} do
+      conn = Conn.put_req_header(conn_base, "authorization", token)
+      assert %{assigns: %{user_id: "someone"}} = VerifyApiToken.call(conn, [])
+    end
+
+    test "returns BadRequest if header doesn't have authorization header", %{conn: conn} do
+      assert %{status: 400} = VerifyApiToken.call(conn, [])
+    end
+
+    test "returns BadRequest for invalid api token", %{conn: conn_base} do
+      conn = Conn.put_req_header(conn_base, "authorization", "invalidtoken")
+      assert %{status: 400} = VerifyApiToken.call(conn, [])
+    end
+  end
+end
