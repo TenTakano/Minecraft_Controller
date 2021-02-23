db7fc7c03f0da8d5cf401d8b761dd02811e02ce7
TenTakano <takano20771@gmail.com>
2021-02-24 01:59:36 +0900

    fix caller

--- test/minecraft_controller_web/controllers/ec2_controller/start_test.exs
+++ test/minecraft_controller_web/controllers/ec2_controller/start_test.exs
@@ -9,7 +9,7 @@ defmodule MinecraftControllerWeb.EC2Controller.StartTest do
   describe "get/2" do
     test "execute start script", %{conn: conn} do
       :meck.expect(EC2, :target_instance_id, fn -> {:ok, "started_instance_id"} end)
-      :meck.expect(EC2, :start_instance, fn _ -> :ok end)
+      :meck.expect(EC2, :start_instance, fn -> :ok end)
       :meck.expect(EC2, :get_instance, fn -> {:ok, %{status: "running", ip: "111.111.111.111"}} end)
       get(conn, @path) |> assert_response(200, %{ip: "111.111.111.111"})
     end
@@ -21,7 +21,7 @@ defmodule MinecraftControllerWeb.EC2Controller.StartTest do
 
     test "returns 409 if wait sequence timeout", %{conn: conn} do
       :meck.expect(EC2, :target_instance_id, fn -> {:ok, "started_instance_id"} end)
-      :meck.expect(EC2, :start_instance, fn _ -> :ok end)
+      :meck.expect(EC2, :start_instance, fn -> :ok end)
       :meck.expect(EC2, :get_instance, fn -> {:ok, %{status: "pending", ip: nil}} end)
       :meck.expect(Utils, :wait_milliseconds, fn _ -> :ok end)
       get(conn, @path) |> assert_error(Error.AwsError)
