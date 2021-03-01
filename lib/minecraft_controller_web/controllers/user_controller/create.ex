6083a24d703574ba95c89f347d5fa835af79f397
TenTakano <takano20771@gmail.com>
2021-03-02 01:24:46 +0900

    impl params validator

--- lib/minecraft_controller_web/controllers/user_controller/create.ex
+++ lib/minecraft_controller_web/controllers/user_controller/create.ex
@@ -4,6 +4,16 @@ defmodule MinecraftControllerWeb.UserController.Create do
   alias MinecraftController.Users
 
   def post(conn, params) do
-    json(conn, params)
+    case validate_params(params) do
+      {:ok, _validated_params} ->
+        json(conn, params)
+      :bad_request ->
+        error_json(conn, Error.BadRequest)
+    end
   end
+
+  defp validate_params(%{"id" => id, "password" => password}) do
+    {:ok, %{id: id, password: password}}
+  end
+  defp validate_params(_), do: :bad_request
 end
