bd110a5c6bc773ff202b852f4a6c899c4f663db8
TenTakano <takano20771@gmail.com>
2021-06-06 17:08:14 +0900

    impl test for success case with do nothing

--- test/minecraft_controller_web/controllers/user_controller/update_test.exs
+++ test/minecraft_controller_web/controllers/user_controller/update_test.exs
@@ -1,7 +1,6 @@
 defmodule MinecraftControllerWeb.UserController.UpdateTest do
   use MinecraftControllerWeb.ConnCase
 
-  alias ExAws.Dynamo
   alias MinecraftController.{Auth, Users}
   alias Users.User
 
@@ -19,14 +18,15 @@ defmodule MinecraftControllerWeb.UserController.UpdateTest do
 
       Users.create_user(user)
 
-      [conn: conn_with_header, user: user]
+      path = @path_base <> "/" <> user.id
+
+      [conn: conn_with_header, user: user, path: path]
     end
 
-    test "updates user attribute", %{conn: conn, user: user_base} do
+    test "updates user attribute", %{conn: conn, user: user_base, path: path} do
       :meck.expect(ExCrypto, :generate_aes_key, fn _, _ -> {:ok, user_base.salt} end)
       request = %{password: "another_pass"}
-      path = @path_base <> "/" <> user_base.id
-      put(conn, path, request) |> assert_response(200, %{id: "someone"})
+      put(conn, path, request) |> assert_response(200, %{id: user_base.id})
 
       expected_user = %User{
         id: user_base.id,
@@ -34,9 +34,16 @@ defmodule MinecraftControllerWeb.UserController.UpdateTest do
         password_hash: Auth.hash_password(request.password, user_base.salt)
       }
 
-      assert Dynamo.get_item("Users", %{id: user_base.id})
-             |> ExAws.request!()
-             |> Dynamo.decode_item(as: User) == expected_user
+      assert {:ok, user} = Users.get_user(user_base.id)
+      assert user == expected_user
+    end
+
+    test "returns 200 with do nothing for empty map", %{conn: conn, user: user_base, path: path} do
+      {:ok, expected_user} = Users.get_user(user_base.id)
+      put(conn, path, %{}) |> assert_response(200, %{id: user_base.id})
+
+      assert {:ok, user} = Users.get_user(user_base.id)
+      assert user == expected_user
     end
   end
 end
