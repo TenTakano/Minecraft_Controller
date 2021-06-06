ad4cbff1b0ea012e368aec909526588d809872e2
TenTakano <takano20771@gmail.com>
2021-06-06 19:30:36 +0900

    impl token validation mock function

--- lib/minecraft_controller_web/plug/verify_api_token.ex
+++ lib/minecraft_controller_web/plug/verify_api_token.ex
@@ -6,6 +6,14 @@ defmodule MinecraftControllerWeb.Plug.VerifyApiToken do
   alias MinecraftControllerWeb.Error.BadRequest
 
   def call(conn, _) do
+    if Mix.env() in [:dev, :prod] do
+      validate_token(conn)
+    else
+      mock_token_validation(conn)
+    end
+  end
+
+  def validate_token(conn) do
     with(
       [token] <- Conn.get_req_header(conn, "authorization"),
       {:ok, user_id} <- Auth.verify_access_token(token)
@@ -13,6 +21,13 @@ defmodule MinecraftControllerWeb.Plug.VerifyApiToken do
       Conn.assign(conn, :user_id, user_id)
     else
       _ -> error_json(conn, BadRequest) |> halt()
+    end
+  end
+
+  defp mock_token_validation(conn) do
+    case Conn.get_req_header(conn, "mocked_user_id") do
+      [] -> conn
+      [user_id] -> Conn.assign(conn, :user_id, user_id)
     end
   end
 end
