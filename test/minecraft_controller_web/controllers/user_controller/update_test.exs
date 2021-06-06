86b4f68baf0a5db6d33e13206c5093177a2cef11
TenTakano <takano20771@gmail.com>
2021-06-06 16:56:35 +0900

    impl test for success case

--- test/minecraft_controller_web/controllers/user_controller/update_test.exs
+++ test/minecraft_controller_web/controllers/user_controller/update_test.exs
@@ -1,11 +1,42 @@
 defmodule MinecraftControllerWeb.UserController.UpdateTest do
   use MinecraftControllerWeb.ConnCase
 
+  alias ExAws.Dynamo
+  alias MinecraftController.{Auth, Users}
+  alias Users.User
+
   @path_base "/api/users"
 
   describe "update/2" do
-    test "updates user", %{conn: conn} do
-      put(conn, @path_base <> "/someone", %{}) |> assert_response(200, %{})
+    setup %{conn: conn} do
+      conn_with_header = put_req_header(conn, "content-type", "application/json")
+
+      user = %{
+        id: "someone",
+        salt: "somasalt",
+        password: "password"
+      }
+
+      Users.create_user(user)
+
+      [conn: conn_with_header, user: user]
+    end
+
+    test "updates user attribute", %{conn: conn, user: user_base} do
+      :meck.expect(ExCrypto, :generate_aes_key, fn _, _ -> {:ok, user_base.salt} end)
+      request = %{password: "another_pass"}
+      path = @path_base <> "/" <> user_base.id
+      put(conn, path, request) |> assert_response(200, %{id: "someone"})
+
+      expected_user = %User{
+        id: user_base.id,
+        salt: user_base.salt,
+        password_hash: Auth.hash_password(request.password, user_base.salt)
+      }
+
+      assert Dynamo.get_item("Users", %{id: user_base.id})
+             |> ExAws.request!()
+             |> Dynamo.decode_item(as: User) == expected_user
     end
   end
 end
